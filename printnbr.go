c245b023d9db970b91bfab9551dd2d9194b3be61
Claudia <c.s@Claudias-MacBook-Pro.local>
2026-05-27 12:44:40 +0100

    Format printnbr

--- printnbr.go
+++ printnbr.go
@@ -3,27 +3,19 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-
 	if n < 0 {
-
 		z01.PrintRune('-')
 		n = -n
-
 	}
 
 	if n == 0 {
-
 		z01.PrintRune('0')
 		return
-
 	}
 
 	if n >= 10 {
-
 		PrintNbr(n / 10)
-
 	}
 
 	z01.PrintRune(rune(n%10) + '0')
-
 }
