aec778defd65cdb198b5b587fb4d400bafe25d2f
Claudia <c.s@Claudias-MacBook-Pro.local>
2026-05-27 08:58:41 +0100

    Format printcomb

--- printcomb.go
+++ printcomb.go
@@ -3,11 +3,8 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-
 	for a := '0'; a <= '7'; a++ {
-
 		for b := a + 1; b <= '8'; b++ {
-
 			for c := b + 1; c <= '9'; c++ {
 
 				z01.PrintRune(a)
@@ -15,18 +12,12 @@ func PrintComb() {
 				z01.PrintRune(c)
 
 				if a != '7' || b != '8' || c != '9' {
-
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
-
 				}
-
 			}
-
 		}
-
 	}
 
 	z01.PrintRune('\n')
-
 }
