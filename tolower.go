58ba44b0512201d09516ccc39ef2d65fa8be80d1
Claudia <c.s@Claudias-MacBook-Pro.local>
2026-06-02 17:06:44 +0100

    Complete tolower

---
+++ tolower.go
@@ -0,0 +1,14 @@
+package piscine
+
+func ToLower(s string) string {
+	result := ""
+
+	for _, r := range s {
+		if r >= 'A' && r <= 'Z' {
+			r = r + 32
+		}
+		result += string(r)
+	}
+
+	return result
+}
